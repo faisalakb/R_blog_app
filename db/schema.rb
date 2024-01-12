7e30cae623faef60470ff3e64c1e8d03cb65c15d
faisalakb <faisalakb786@gmail.com>
2024-01-12 13:05:09 +0500

    Add model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_11_134942) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_11_18_075731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.text "text"
     t.integer "user_id"
     t.integer "post_id"
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 2024_01_11_134942) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
@@ -40,8 +39,8 @@ ActiveRecord::Schema.define(version: 2024_01_11_134942) do
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.integer "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -50,8 +49,8 @@ ActiveRecord::Schema.define(version: 2024_01_11_134942) do
     t.string "photo"
     t.text "bio"
     t.integer "posts_counter", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts", on_delete: :cascade
