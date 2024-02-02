1c822345d5df15a9172bf22a53e913beb5ee0b8e
faisalakb <faisalakb786@gmail.com>
2024-02-02 21:30:55 +0500

    Add endpoints

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,24 @@
+class Api::CommentsController < ApplicationController
+  before_action :set_user_and_post, only: [:index, :create]
+
+  def index
+    comments = @post.comments
+    render json: { user_id: @user.id, post_id: @post.id, comments: comments }
+  end
+
+  def create
+    comment = @post.comments.create(comment_params.merge(user: @user))
+    render json: comment, status: :created
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_user_and_post
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,20 @@ Rails.application.routes.draw do
     end
   end
 
+  # API routes
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index]
+    end
+    resources :posts, only: [] do
+      resources :comments, only: [:index, :create]
+    end
+
+    get '/users/:user_id/posts/:post_id/comments', to: 'comments#index'
+    post '/users/:user_id/posts/:post_id/comments', to: 'comments#create'
+
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
